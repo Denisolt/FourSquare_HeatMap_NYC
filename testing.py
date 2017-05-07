808abd0dbb35b35dd5bac524a467fcb3aa5a0c79
Deni Shakh <admin@MacBook-dl.local>
2017-05-07 18:32:14 -0400

    final_upload

---
+++ testing.py
@@ -0,0 +1,21 @@
+import os,csv
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+import pandas as pd
+
+#data = pd.read_csv('data.csv')
+#location = data.latitude.astype(str) + ", " + data.longitude.astype(str)
+#print location
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    data = pd.read_csv('data.csv')
+    location = data.latitude.astype(str) + ", " + data.longitude.astype(str)
+    return render_template('index.html', x = location)
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
